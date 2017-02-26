dbbeb21b642c8790426210fb971fc9e1c4f538a5
jiro <gitromaru666>
2017-02-27 00:25:18 +0900

    add multi languages prototype

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,32 @@
 package app;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Locale;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import util.ResourceBundleWithUtf8;
 
 public class Main extends Application {
+  public static final String TITLE = "TKoolFacetileEditor";
   @Override
   public void start(Stage primaryStage) {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+    URL location = getClass().getResource("Main.fxml");
+    ResourceBundle resources = ResourceBundle.getBundle(
+        "app.res.langs.Main"
+        , Locale.getDefault()
+        , ResourceBundleWithUtf8.UTF8_ENCODING_CONTROL
+        );
+    FXMLLoader loader = new FXMLLoader(location, resources);
+
     try {
       VBox root = (VBox) loader.load();
       Scene scene = new Scene(root, 200, 300);
-      scene.getStylesheets().add(getClass().getResource("./res/Basic.css").toExternalForm());
+      scene.getStylesheets().add(getClass().getResource("res/css/Basic.css").toExternalForm());
       primaryStage.setScene(scene);
       primaryStage.setTitle(TITLE);
 
---
+++ src/app/MainController.java
@@ -0,0 +1,13 @@
+package app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class MainController {
+  @FXML private MenuItem fileMenuItem;
+
+  @FXML
+  private void initialize() {
+
+  }
+}
