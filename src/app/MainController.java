b8875fe81c4b826c9101d185fea59343964d920d
jiro <gitromaru666>
2017-02-27 11:22:01 +0900

    add resource bundle value

--- src/app/MainController.java
+++ src/app/MainController.java
@@ -9,14 +9,35 @@ import javafx.scene.layout.*;
 
 public class MainController {
   // FXMLで指定するコンポーネント{{{
+  // メニューバー {{{
   @FXML private MenuItem fileMenuItem;
+  //}}}
+  // ファイルリスト//{{{
+  @FXML private TitledPane fileListTitledPane;
+  @FXML private ListView<String> fileListView;
+  @FXML private Button clearImagesButton;
+  @FXML private Button deleteListButton;
+  @FXML private Button clearListButton;
+  //}}}
+  // パネル操作変更//{{{
+  @FXML private TitledPane controlPanelTitledPane;
+  @FXML private RadioButton deleteModeRadioButton;
+  @FXML private RadioButton deleteNonEmptyModeRadioButton;
+  @FXML private RadioButton sortModeRadioButton;
+  @FXML private RadioButton reverseModeRadioButton;
+  //}}}
+  // 出力画像パネル//{{{
   @FXML private TitledPane outputImageTitledPane;
   @FXML private GridPane outputImageGridPane;
+  //}}}
   //}}}
 
   @FXML
   private void initialize() {//{{{
     // TEST_CODE//{{{
+    // ファイルをリストビューに追加する//{{{
+    //}}}
+    // プリセットファイルから出力画像のレイ・アウトを変更する。//{{{
     Properties prop = new Properties();
     try (InputStream is = new FileInputStream(new File("presets/MV.properties"))) {
       prop.load(new InputStreamReader(is, "UTF-8"));
@@ -37,6 +58,7 @@ public class MainController {
       IntStream.range(0, count)
         .forEach(i -> {
           final Button button = new Button("" + (i+1));
+          //button.setStyle("-fx-background-color: black;");
           double width = (double) size;
           button.setPrefWidth(width);
           button.setPrefHeight(width);
@@ -49,6 +71,7 @@ public class MainController {
     } catch (IOException e) {
       e.printStackTrace();
     }
+//}}}
     //}}}
   }//}}}
 }
