ea9891f0ecdee066d79b4188107d6949ac663898
jiro <gitromaru666>
2017-03-01 11:50:51 +0900

    refactoring

--- src/app/image/ReverseStrategy.java
+++ src/app/image/ReverseStrategy.java
@@ -6,6 +6,9 @@ import java.util.stream.IntStream;
 import javafx.scene.image.*;
 
 public class ReverseStrategy implements ControlOutputPaneStrategy {
+  // 画像処理の書式(ARGB)
+  private static final WritablePixelFormat<IntBuffer> FORMAT = WritablePixelFormat.getIntArgbInstance();
+
   /**
    * 選択したペインの画像を左右反転する。
    *
@@ -13,34 +16,63 @@ public class ReverseStrategy implements ControlOutputPaneStrategy {
    */
   @Override
   public void invoke(List<StackImageView> list) {
-    // TODO
+
     list.stream()
       .forEach(siv -> {
-        Image image        = siv.getImage();
-        PixelReader reader = image.getPixelReader();
+        Image image = siv.getImage();
+        int width   = (int) image.getWidth();
+        int height  = (int) image.getHeight();
 
-        int width  = (int) image.getWidth();
-        int height = (int) image.getHeight();
+        int[] pixels         = readPixels(image, width, height);
+        int[] reversedPixels = createReversedPixels(pixels, width);
+        WritableImage wImage = writePixels(reversedPixels, width, height);
 
-        WritableImage wImage = new WritableImage(width, height);
-        PixelWriter writer   = wImage.getPixelWriter();
-
-        WritablePixelFormat<IntBuffer> format = WritablePixelFormat.getIntArgbInstance();
-        int[] pixels = new int[width * height];
-        reader.getPixels(0, 0, width, height, format, pixels, 0, width);
-
-        // pixelの左右反転処理//{{{
-        int[] reversedPixels = new int[pixels.length];
-        for (int i=0; i<pixels.length; i++) {
-          int a = (i + width) / width * width;
-          int reverseIndex = a + (i / width * width) - i - 1;
-          reversedPixels[reverseIndex] = pixels[i];
-        }
-        //}}}
-
-        writer.setPixels(0, 0, width, height, format, reversedPixels, 0, width);
         siv.setImage(wImage);
       });
   }
+
+  /**
+   * 対象画像がらpixel画素配列を取得する。
+   *
+   * @param image 対象画像
+   * @return 対象画像のint配列
+   */
+  private int[] readPixels(Image image, int width, int height) {//{{{
+    int[] pixels = new int[width * height];
+    PixelReader reader = image.getPixelReader();
+    reader.getPixels(0, 0, width, height, FORMAT, pixels, 0, width);
+    return pixels;
+  }//}}}
+  /**
+   * 渡したpixel画素から左右反転したpixel画素を新たに生成して返す。
+   *
+   * @param pixels 左右反転させる画素
+   * @param width 画像の幅
+   * @return 左右反転された新たな画像
+   */
+  private int[] createReversedPixels(int[] pixels, int width) {//{{{
+    int[] reversedPixels = new int[pixels.length];
+    for (int i=0; i<pixels.length; i++) {
+      int a = (i + width) / width * width;
+      int b = i / width * width;
+      int reverseIndex = a + b - i - 1;
+      reversedPixels[reverseIndex] = pixels[i];
+    }
+    return reversedPixels;
+  }//}}}
+  /**
+   * 渡したpixel配列を書き込んだ新しい画像を返す。
+   *
+   * @param pixels 書き込むpixel画素
+   * @param width 画像の横幅
+   * @param height 画像の縦幅
+   * @return pixelが書き込まれた新しい画像
+   */
+  private WritableImage writePixels(int[] pixels, int width, int height) {//{{{
+    WritableImage wImage = new WritableImage(width, height);
+    PixelWriter writer   = wImage.getPixelWriter();
+    writer.setPixels(0, 0, width, height, FORMAT, pixels, 0, width);
+    return wImage;
+  }//}}}
 }
 
